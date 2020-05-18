e71851f5aa86cc5bdb9313f30388028ce880a61f
malik-i <malikiscanedu@gmail.com>
2020-05-17 22:13:56 -0700

    updated recommended settings

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -13,7 +13,7 @@ class WeatherViewController: UIViewController {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "be5947570e6ad4231d4477a724d17657"
     
 
     //TODO: Declare instance variables here
